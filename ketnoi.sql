d9683f1672f9413530ab3199281fe86030a4dd57
legiahan311002 <Legiahan311002@gmail.com>
2022-03-15 22:38:49 +0700

    them file ketnoi.sql

---
+++ ketnoi.sql
@@ -0,0 +1,18 @@
+﻿Create database Information
+Go
+
+use Information 
+go
+
+Create table Users 
+(
+	accName nvarchar(50) primary key,
+	accMoney money,
+	descriptional nvarchar(100)
+)
+GO
+
+insert into Users 
+	values	('Trịnh Thế Công', 10000000, 'Sinh viên'),
+			('Nguyễn Văn Chương', 1000, 'Sinh viên'),
+			('Nguyễn Trần Trung Anh', 1000000, 'Sinh viên')
